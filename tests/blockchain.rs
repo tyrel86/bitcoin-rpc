9eb5ecd8c312ea6e6998e79dd722f33e2b738ea4
Jean Pierre De Jesus Dudey Diaz <jeandudey@hotmail.com>
2016-04-16 23:39:37 -0430

    test(blockchain): Make some code pass.

--- tests/blockchain.rs
+++ tests/blockchain.rs
@@ -4,13 +4,11 @@ use bitcoinrpc::BitcoinRpc;
 
 #[test]
 fn test_blockchain_methods() {
-    let client = BitcoinRpc::new("localhost:18332", Some(String::from("user")), Some(String::from("pass")));
+    let client = BitcoinRpc::new("http://localhost:18332", Some(String::from("user")), Some(String::from("pass")));
 
     client.getbestblockhash().unwrap();
-    client.getblock(String::from(""), false).unwrap();
     client.getblockchaininfo().unwrap();
     client.getblockcount().unwrap();
-    client.getblockhash(0).unwrap();
     client.getchaintips().unwrap();
     client.getdifficulty().unwrap();
     client.getmempoolinfo().unwrap();
